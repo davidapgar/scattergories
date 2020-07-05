2bbe8e40ffa0f73b21bc4f89f81516ee3fd347a5
David Apgar <davidapgar@gmail.com>
2020-07-05 14:39:00 -0700

    Simple count down timer

--- site/main.js
+++ site/main.js
@@ -1,1 +1,35 @@
 console.log('hello world');
+const gameTimer = document.getElementById('gameTimer');
+
+class CountDown {
+  constructor() {
+    this.count = 10;
+    this.intervalID = null;
+  }
+
+  get running() {
+    return (this.intervalID !== null);
+  }
+
+  start() {
+    this.count = 10;
+    this.intervalID = window.setInterval(timeTick, 1000);
+  }
+}
+
+gameTimer.textContent = "111";
+let countDown = new CountDown();
+console.log("Is running? " + countDown.running);
+
+function timeTick() {
+  if (countDown.count <= 0 && countDown.running) {
+    // Stop the timer.
+    window.clearInterval(this.intervalID);
+  } else {
+    countDown.count -= 1;
+    gameTimer.textContent = countDown.count;
+  }
+}
+
+gameTimer.textContent = countDown.count;
+countDown.start();
